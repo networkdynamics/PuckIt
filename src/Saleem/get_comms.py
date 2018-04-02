71f0b658e4e9e94bfb4e254c21055e734d2c07c2
Haji Saleem <haji.saleem@mail.mcgill.ca>
2018-04-02 18:58:03 -0400

    start extracting subreddit comments 💛

---
+++ src/Saleem/get_comms.py
@@ -0,0 +1,73 @@
+'''
+@uthor: Saleem
+Date: April 2, 2018
+
+Gather comments that belong to the NHL subreddits
+'''
+
+#Imports
+import os
+import time
+import json
+import gzip
+import multiprocessing
+from pprint import pprint
+
+
+out_path = '/home/ndg/users/hsalee/PuckIt/temp'
+raw_path = '/home/ndg/arc/reddit'
+
+years = ['2016', '2017', '2018']
+
+all_files = []
+
+for year in years:
+    data_path = os.path.join(raw_path, year)
+    files = sorted(os.listdir(data_path))
+    files = [x for x in files if 'RC' in x]
+    if year == '2016':
+        files = [x for x in files if 'RC_2016-09' in x or 'RC_2016-10' in x or 'RC_2016-11' in x or 'RC_2016-12' in x]
+    #files = [os.path.join(data_path, x) for x in files]
+    all_files.extend(files)
+
+num = len(all_files)
+
+sub_file = '/home/ndg/users/hsalee/PuckIt/resources/nhl_subs.txt'
+with open(sub_file, 'r') as fin:
+    all_subs = fin.readlines()
+all_subs = set([x.strip() for x in all_subs])
+
+def get_subs(file_name):
+    print file_name, all_files.index(file_name), 'of', num
+    year = file_name[3:7]
+    file_path = os.path.join(raw_path, year, file_name)
+
+    with gzip.open(file_path, 'r') as fin:
+        all_lines = fin.readlines()
+
+
+    to_write = []
+    for line in all_lines:
+        jobj = json.loads(line)
+        sub = jobj['subreddit']
+        if sub in all_subs:
+            to_write.append(line)
+
+    out_file_name = file_name[:-3]
+    out_file_path = os.path.join(out_path, out_file_name)
+    with open(out_file_path, 'w') as fp:
+        for line in to_write:
+            fp.write(line)
+    return
+
+
+#Multiprocessing Pool
+
+def mp_sampler():
+    p = multiprocessing.Pool(20)
+    p.map(get_subs, all_files)
+
+#Run it
+
+if __name__ == '__main__':
+    mp_sampler()
