e6bf54a6f77e961f8261a60d1b21af889dd74c19
Haji Saleem <haji.saleem@mail.mcgill.ca>
2018-04-02 17:43:45 -0400

    extracting subreddit data ❤️

---
+++ src/Saleem/count_subs.py
@@ -0,0 +1,34 @@
+'''
+extract the subscriber count for each sub
+'''
+
+
+import os
+import time
+import requests
+import json
+
+headers = {"User-Agent": "lets get these pages"}
+
+teampath = '/home/ndg/users/hsalee/PuckIt/resources/nhl_teams.txt'
+data_dir = '/home/ndg/projects/shared_datasets/PuckIt/data'
+
+
+with open(teampath, 'r') as fin:
+    all_teams = fin.readlines()
+all_teams = [x.strip() for x in all_teams]
+
+def get_about(team_name):
+    team_dir = team_name.replace(" ", "_")
+    team_path = os.path.join(data_dir, team_dir)
+    file_name = 'about.json'
+    file_path = os.path.join(team_path, file_name)
+    with open(file_path, 'r') as fp:
+        content = json.load(fp)
+    print content['data']['subscribers']
+    return
+
+
+
+for team in all_teams:
+    get_about(team)
