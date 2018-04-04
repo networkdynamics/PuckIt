be7daff85c67e29e87b74c078c72d42d7e70b71c
Caitrin Armstrong <caitrin.armstrong@mail.mcgill.ca>
2018-04-04 19:55:22 -0400

    finished network stats

--- src/Caitrin/NetworkStats.py
+++ src/Caitrin/NetworkStats.py
@@ -3,8 +3,10 @@ from collections import defaultdict
 import sys
 import csv
 import json
+import networkx as nx
+#import community
 
-def construct_network_graph(sub_dir):
+def construct_edgelist(sub_dir):
     id_author = {}
     comment_file = sub_dir + "reddit_comments.txt"
     with open(comment_file, "rb") as infile:
@@ -30,7 +32,7 @@ def construct_network_graph(sub_dir):
                 user_pairs[(parent, child)] += 1
 
 
-    network_file = sub_dir + "network_file.txt"
+    network_file = sub_dir + "edgelist.txt"
 
     with open(network_file, "wb") as outfile:
 
@@ -39,7 +41,77 @@ def construct_network_graph(sub_dir):
             csvwriter.writerow([pairing[0], pairing[1], user_pairs[pairing]])
 
 
+def construct_networkx_graph(sub_dir):
+    edgelist_file = sub_dir + "edgelist.txt"
+
+    G = nx.read_edgelist(edgelist_file, delimiter=",", create_using=nx.DiGraph(), data=(('weight',int),))
+
+    return G
+
+def get_clustering_coefficient(G):
+    GU = G.to_undirected()
+
+    return nx.average_clustering(GU)
+
+def get_assortativity_coef(G):
+    return nx.degree_assortativity_coefficient(G, x='in', y='out', weight='weight')
+
+def get_attribute_coef(G):
+    print "attribute:"
+    return nx.attribute_assortativity_coefficient(G, 'weight')
+
+def get_p_corr_coef(G):
+    return nx.degree_pearson_correlation_coefficient(G, x='in', y='out', weight='weight')
+
+
+def get_neighbour_degree(G):
+    return nx.average_neighbor_degree(G, source='in', target='out', weight='weight')
+
+#need to turn the graph undirected
+def get_comm(G):
+    GU = G.to_undirected()
+    return nx.communicability(GU)
+
+def get_clique_size(G):
+    GU = G.to_undirected()
+    return nx.graph_clique_number(GU)
+
+def get_number_of_cliques(G):
+    GU = G.to_undirected()
+    return nx.graph_number_of_cliques(GU)
+
+def get_triangles(G):
+    GU = G.to_undirected()
+    return nx.triangles(GU)
+
+def get_transitivity(G):
+    return nx.transitivity(G)
+
+def get_is_strongly_connected(G):
+    return nx.is_strongly_connected(G)
+
+def get_number_strongly_connected_components(G):
+    return nx.is_strongly_connected(G)
+
+def get_is_weakly_connected(G):
+    return nx.is_weakly_connected(G)
+
+def get_number_weakly_connected_components(G):
+    return nx.number_weakly_connected_components(G)
+
+#need to make undirected
+def get_number_connected_components(G):
+    GU = G.to_undirected()
+    return nx.number_connected_components(GU)
+
+#def get_modularity(G):
+#    part = community.best_partition(G)
+#    mod = community.modularity(part,G)
+
+
 sub_dir = sys.argv[1]
 
-construct_network_graph(sub_dir)
-
+#construct_edgelist(sub_dir)
+G = construct_networkx_graph(sub_dir)
+#print "subreddit", "clustering", "assortativity", "pearson_corr_coef", "clique_size", "number_of_cliques", "transitivity", "is_strongly_connected", "is_weakly_connected", "number_strong_comp", "number_weak_comp"
+#print sub_dir.split("/")[-2], get_clustering_coefficient(G), get_assortativity_coef(G), get_p_corr_coef(G), get_clique_size(G), get_number_of_cliques(G), get_transitivity(G), get_is_strongly_connected(G), get_is_weakly_connected(G), get_number_strongly_connected_components(G), get_number_weakly_connected_components(G)
